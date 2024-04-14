dda8a4ac39a9239dddca82eddee3bb25a8bc7dfd
josh <74937807+d-zah@users.noreply.github.com>
2024-04-14 00:18:45 -0400

    added more dialogue & fixed tile gap issue

--- ElevatorRPG/Assets/DialogueSystem/ConversationStart.cs
+++ ElevatorRPG/Assets/DialogueSystem/ConversationStart.cs
@@ -6,15 +6,20 @@ public class ConversationStart : MonoBehaviour
 {
     public Conversation convo;
     private PlayerController playerController;
+    [SerializeField] private bool canTalkToTwice;
+    [SerializeField] private bool hasTalkedTo;
 
     public void Awake(){
+        hasTalkedTo = false;
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     public void OnTriggerEnter2D(Collider2D other) {
+        if(!canTalkToTwice && hasTalkedTo) return;
         if(other.CompareTag("Player") && !other.isTrigger) {
             playerController.playerCanMove(true);
             DialogueManager.StartConversation(convo);
+            hasTalkedTo = true;
         }
     }
 }
