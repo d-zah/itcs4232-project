4c76d0fa9559c9eaa9109d1c7384bd57f92901d0
josh <74937807+d-zah@users.noreply.github.com>
2024-04-16 00:49:55 -0400

    made dialogue boxes interactable

--- ElevatorRPG/Assets/DialogueSystem/ConversationStart.cs
+++ ElevatorRPG/Assets/DialogueSystem/ConversationStart.cs
@@ -7,19 +7,44 @@ public class ConversationStart : MonoBehaviour
     public Conversation convo;
     private PlayerController playerController;
     [SerializeField] private bool canTalkToTwice;
+    [SerializeField] private bool requiresEPress;
     [SerializeField] private bool hasTalkedTo;
+    private bool isTalkingTo;
+    public GameObject pressEPrompt;
+    private bool isInTrigger;
 
     public void Awake(){
+        isInTrigger = false;
         hasTalkedTo = false;
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     public void OnTriggerEnter2D(Collider2D other) {
-        if(!canTalkToTwice && hasTalkedTo) return;
         if(other.CompareTag("Player") && !other.isTrigger) {
-            playerController.playerCanMove(true);
-            DialogueManager.StartConversation(convo);
-            hasTalkedTo = true;
+            isInTrigger = true;
+            if(requiresEPress) pressEPrompt.SetActive(true);
         }
+    }
+
+    public void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player") && !other.isTrigger) {
+            isInTrigger = false;
+            isTalkingTo = false;
+            if(requiresEPress) pressEPrompt.SetActive(false);
+        }
+    }
+
+    public void Update(){
+        if(!isInTrigger) return;
+        if(!canTalkToTwice && hasTalkedTo) return;
+        if(isTalkingTo) return;
+        if(!Input.GetKeyDown(KeyCode.E) && requiresEPress) return;
+        
+        playerController.playerCanMove(true);
+        DialogueManager.StartConversation(convo);
+        pressEPrompt.SetActive(false);
+        isTalkingTo = true;
+        hasTalkedTo = true;
+        
     }
 }
